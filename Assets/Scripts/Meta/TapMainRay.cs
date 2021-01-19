2390cd5aed17edadc1ff2834bc533c7619a1caee
s181034-MoriJellyfish <52020604+s181034-MoriJellyfish@users.noreply.github.com>
2021-01-19 20:04:40 +0900

    Refactoring

--- Assets/Scripts/Meta/TapMainRay.cs
+++ Assets/Scripts/Meta/TapMainRay.cs
@@ -9,7 +9,6 @@ public class TapMainRay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TouchSupported:" + Input.touchSupported);
 
     }
 
@@ -24,63 +23,83 @@ public class TapMainRay : MonoBehaviour
                 Vector3 fingerpos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, -10.0f));
                 fingerpos.z = 0;
                 Vector3 raystart = (Vector3.Normalize(fingerpos - new Vector3(0, 5, 0)) * 9) + new Vector3(0, 5, 0);
-                var hit = Physics2D.RaycastAll(new Vector2(raystart.x, raystart.y), new Vector2(0, 5) - new Vector2(raystart.x, raystart.y),100f,1<<8);//note
+                var hit = Physics2D.RaycastAll(new Vector2(raystart.x, raystart.y), new Vector2(0, 5) - new Vector2(raystart.x, raystart.y),100f);//note
 
-                //(Debug)DrawRay
-                if (hit.Length != 0)
-                {
-                    if (touch.phase == TouchPhase.Began)
-                    {
-                        Debug.DrawRay(raystart, new Vector3(0, 5, 0) - raystart, Color.red, 0.01f);
-                    }
-                    else
-                    {
-                        Debug.DrawRay(raystart, new Vector3(0, 5, 0) - raystart, Color.yellow, 0.01f);
-                    }
-                }
-                else
-                {
-                    Debug.DrawRay(raystart, new Vector3(0, 5, 0) - raystart, Color.blue, 0.01f);
-                }
+                DrawRay(hit,raystart);//debug
                 
                 if (hit.Length != 0)
                 {
                     //tap
-                    if (touch.phase == TouchPhase.Began) {
-                        float[] times=new float[hit.Length];
-                        int i=0;
-                        foreach(var obj in hit)
-                        {
-                            times[i]=obj.transform.gameObject.GetComponent<TapNoteMesh>().LandingTime;
-                            i++;
-                        }
-                        float min = times.Min();
-                        int minkey = Array.IndexOf(times, min);
-
-                        while (minkey!=-1)
-                        {
-                            hit[minkey].transform.gameObject.GetComponent<TapNoteEvent>().Tap();
-                            times[minkey] = times[minkey]+1;
-                            minkey = Array.IndexOf(times, min);
-                            
-                        }
-
+                    if (touch.phase == TouchPhase.Began)
+                    {
+                        TapnoteTap(LayerSort(hit, 8));//Tapnotes
                     }
                 }
 
 
-                hit = Physics2D.RaycastAll(new Vector2(raystart.x, raystart.y), new Vector2(0, 5) - new Vector2(raystart.x, raystart.y), 100f, 1 << 31);
-                if (hit.Length != 0)
+                var keys = LayerSort(hit, 31);
+                foreach(var obj in keys)
                 {
-                    foreach(var obj in hit)
-                    {
-                        obj.transform.parent.GetComponent<Keybeam>().hold = true;
-
-                    }
+                    obj.transform.parent.GetComponent<Keybeam>().hold = true;
                 }
 
             }
         }
+    }
+
+    void DrawRay(RaycastHit2D[] hit,Vector3 raystart)
+    {
+        if (hit.Length != 0)
+        {
+            Debug.DrawRay(raystart, new Vector3(0, 5, 0) - raystart, Color.yellow, 0.01f);
+
+        }
+        else
+        {
+            Debug.DrawRay(raystart, new Vector3(0, 5, 0) - raystart, Color.blue, 0.01f);
+        }
 
     }
+
+    GameObject[] LayerSort(RaycastHit2D[] hit,int layer)
+    {
+        GameObject[] objs=new GameObject[hit.Length];
+        int i=0;
+        foreach(var obj in hit)
+        {
+            if (obj.transform.gameObject.layer == layer)
+            {
+                objs[i] = obj.transform.gameObject;
+                i++;
+            }
+        }
+        Array.Resize(ref objs, i);
+        return objs;
+    }
+    
+    void TapnoteTap(GameObject[] taps)
+    {
+        if (taps.Length != 0)
+        {
+            float[] times = new float[taps.Length];
+            int i = 0;
+            foreach (var obj in taps)
+            {
+                times[i] = obj.GetComponent<TapNoteMesh>().LandingTime;
+                i++;
+            }
+            float min = times.Min();
+            int minkey = Array.IndexOf(times, min);
+
+            while (minkey != -1)
+            {
+                taps[minkey].GetComponent<TapNoteEvent>().Tap();
+                times[minkey] = times[minkey] + 1;
+                minkey = Array.IndexOf(times, min);
+
+            }
+        }
+    }
+
+
 }
